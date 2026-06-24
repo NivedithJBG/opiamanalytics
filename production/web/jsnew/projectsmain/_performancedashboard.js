9e82b181aad8a3816e075a444654a812d414eea7
Opiam Analytics <cmd@geotech.net.in>
2026-06-24 16:02:37 +0530

    Project Cost bar: align to same natural position as Activity Cost bars (remove centering)

--- production/web/jsnew/projectsmain/_performancedashboard.js
+++ production/web/jsnew/projectsmain/_performancedashboard.js
@@ -93,8 +93,6 @@ function renderCdBars(){
     var totalCost = groupItems.reduce(function(s, g){ return s + g.cost; }, 0);
 
     renderCostBars('cd-c2', [{name: _cdProjectName || 'Project', cost: totalCost, id: 0}], null);
-    var c2el = document.getElementById('cd-c2');
-    if (c2el) { c2el.style.display = 'flex'; c2el.style.flexDirection = 'column'; c2el.style.justifyContent = 'center'; c2el.style.paddingBottom = '10%'; }
     renderCostBars('cd-c1', groupItems, filterByGroupCd);
     if (_groups.length) filterByGroupCd(_groups[0].id);
 }
