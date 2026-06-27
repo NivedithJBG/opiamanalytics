17d37baf6adcfa3d11ac661d6f06b76e0bedff88
Opiam Analytics <cmd@geotech.net.in>
2026-06-27 23:13:27 +0530

    IOW bars: fix overlay % to be relative to bar width (acoa/est), orange at left:100%

--- production/web/jsnew/projectsmain/_performancedashboard.js
+++ production/web/jsnew/projectsmain/_performancedashboard.js
@@ -1245,11 +1245,11 @@ function renderSimpleCostBars(containerId, items, onRowClick, showOverlay, getTo
         var est  = r.cost || 0;
         var acoa = showOverlay ? (r.acoa || 0) : 0;
         var pct     = (est / maxVal * 100).toFixed(1);
-        var acoaPct = Math.min(acoa / maxVal * 100, parseFloat(pct)).toFixed(1);
+        var acoaInnerPct = est > 0 ? Math.min(acoa / est * 100, 100).toFixed(1) : '0';
         var bar = est > 0
             ? '<div class="bs" style="width:'+pct+'%;background:#4A5568;position:relative;overflow:visible;">'
-              + (acoa > 0 ? '<div style="position:absolute;top:0;left:0;height:100%;width:'+acoaPct+'%;background:#78909C;border-radius:2px 0 0 2px;opacity:0.9;"></div>' : '')
-              + (acoa > est ? '<div style="position:absolute;top:0;left:'+pct+'%;height:100%;width:'+((acoa-est)/maxVal*100).toFixed(1)+'%;background:#FF7043;border-radius:0 2px 2px 0;"></div>' : '')
+              + (acoa > 0 ? '<div style="position:absolute;top:0;left:0;height:100%;width:'+acoaInnerPct+'%;background:#78909C;border-radius:2px 0 0 2px;opacity:0.9;"></div>' : '')
+              + (acoa > est ? '<div style="position:absolute;top:0;left:100%;height:100%;width:'+((acoa-est)/est*100).toFixed(1)+'%;background:#FF7043;border-radius:0 2px 2px 0;"></div>' : '')
               + '</div>'
             : '<div class="bs" style="width:2%;background:#ccc;"></div>';
         html += '<div class="brow" data-aid="'+r.id+'" style="cursor:pointer;display:flex;align-items:center;">'
