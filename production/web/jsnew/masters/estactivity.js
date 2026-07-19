fb2f63fb7d4a06b26c4429f22901a0a862200bbd
Opiam Analytics <cmd@geotech.net.in>
2026-07-19 13:47:30 +0530

    Fix IOW group not showing in edit mode: set value before alRefreshActIowGroupDropdown

--- production/web/jsnew/masters/estactivity.js
+++ production/web/jsnew/masters/estactivity.js
@@ -288,6 +288,9 @@ $(document).on('click', '.al-edit-activity-btn', function(){
                 $('#editingActivityId').val(idval);
                 $('#saveestactivity').html('<span class="icon-check"></span> Update Activity');
                 $('#alAddActivityTitle').text('Edit Activity');
+                /* set IOW group value before refresh so alRefreshActIowGroupDropdown restores it */
+                if(act.iow_group_id) $('#actIowGroupId').val(act.iow_group_id);
+                if(typeof alRefreshActIowGroupDropdown === 'function') alRefreshActIowGroupDropdown();
 
                 /* rebuild task rows */
                 $('#task-rows-container').empty();
